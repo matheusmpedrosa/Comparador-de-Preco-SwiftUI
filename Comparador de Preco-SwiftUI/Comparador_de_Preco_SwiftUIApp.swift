2caa35b9157d96773be0fdaff3bd5bb6c6283f3f
Matheus Pedrosa <tqi_mpedrosa@uolinc.com>
2025-01-02 21:28:12 -0300

    Initial Commit

---
+++ Comparador de Preco-SwiftUI/Comparador_de_Preco_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Comparador_de_Preco_SwiftUIApp.swift
+//  Comparador de Preco-SwiftUI
+//
+//  Created by Matheus Malcher Pedrosa on 02/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Comparador_de_Preco_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Comparador de Preco-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Comparador de Preco-SwiftUI
+//
+//  Created by Matheus Malcher Pedrosa on 02/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
